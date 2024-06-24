3f3418a464c3c2812fa89b0123c82e9b9df98595
josesantana.web <josesantana.web@gmail.com>
2024-06-24 19:40:30 -0400

    refactor: Extract Review component for reusability

--- src/components/About/Reviews/index.tsx
+++ src/components/About/Reviews/index.tsx
@@ -2,6 +2,35 @@ import { REVIEWS } from '@/data/reviews'
 import { BiSolidStar } from 'react-icons/bi'
 import Slider from 'react-slick'
 
+interface ReviewProps {
+  review: {
+    id: number
+    name: string
+    review: string
+    image: string
+  }
+}
+const Review = ({ review }: ReviewProps) => {
+  return (
+    <div className="p-8 mx-3 bg-white rounded-lg">
+      <p className="mb-4 text-xs text-center text-gray-500">{review.review}</p>
+      <div className="flex items-center justify-center gap-3">
+        <img src={`/images/team/${review.image}`} className="w-10 h-10 rounded-full" />
+        <div className="flex flex-col">
+          <h4 className="text-sm font-semibold">{review.name}</h4>
+          <span className="flex items-center gap-1 text-yellow-300">
+            <BiSolidStar size={18} />
+            <BiSolidStar size={18} />
+            <BiSolidStar size={18} />
+            <BiSolidStar size={18} />
+            <BiSolidStar size={18} />
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const Reviews = () => {
   const settings = {
     dots: true,
@@ -28,22 +57,7 @@ const Reviews = () => {
         <Slider {...settings} className="flex gap-5">
           {REVIEWS.map((review) => (
             <div key={review.id}>
-              <div className="p-8 mx-3 bg-white rounded-lg">
-                <p className="mb-4 text-xs text-center text-gray-500">{review.review}</p>
-                <div className="flex items-center justify-center gap-3">
-                  <img src={`/images/team/${review.image}`} className="w-10 h-10 rounded-full" />
-                  <div className="flex flex-col">
-                    <h4 className="text-sm font-semibold">{review.name}</h4>
-                    <span className="flex items-center gap-1 text-yellow-300">
-                      <BiSolidStar size={18} />
-                      <BiSolidStar size={18} />
-                      <BiSolidStar size={18} />
-                      <BiSolidStar size={18} />
-                      <BiSolidStar size={18} />
-                    </span>
-                  </div>
-                </div>
-              </div>
+              <Review review={review} />
             </div>
           ))}
         </Slider>
